e4862e0c047192aed9da2c9e2a619e5d4e8c645b
Christopher Codrington <chcodrington@gmail.com>
2021-01-07 21:27:04 -0500

    Changed default color scheme
    The theme is based on the vim-pencil-theme

--- config.def.h
+++ config.def.h
@@ -96,30 +96,33 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#212121",
+	"#c30771",
+	"#10a778",
+	"#a89c14",
+	"#008ec4",
+	"#523c79",
+	"#20a5ba",
+	"#d9d9d9",
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#424242",
+	"#fb007a",
+	"#5fd7af",
+	"#f3e430",
+	"#20bbfc",
+	"#6855de",
+	"#4fb8cc",
+	"#424242",
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
+  "#f1f1f1",
+  "#424242",
+  "#20bbfc",
 };
 
 
@@ -127,9 +130,9 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
+unsigned int defaultfg = 259;
+unsigned int defaultbg = 258;
+static unsigned int defaultcs = 260;
 static unsigned int defaultrcs = 257;
 
 /*
